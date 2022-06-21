03c38945a3005458939531c51fb515d829f3fc25
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-06-21 21:17:47 +0500

    Displaying the text of selected note has been described

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
+import tkinter.scrolledtext as tkst
+from turtle import width
 from typing import List
+
+from handbookapi import handbookapi
 
 
 VERSION_ = '0.1.0-alpha'
@@ -40,6 +44,46 @@ class MainFrame(tk.Canvas):
         self.place(x=0, y=0)
 
 
+class NoteTextFrame(tk.Canvas):
+    width: int
+    height: int
+
+    def __init__(self, master: MainFrame, indent: int):
+        self.width = int(master.width * 0.65)
+        self.height = master.height - 10
+
+        super().__init__(master, width=self.width, height=self.height)
+
+        self.place(x=indent, y=0)
+
+
+class NoteTextArea(tkst.ScrolledText):
+    width: int
+    height: int
+    note_id: int
+    text: str
+
+    def __init__(self, master: NoteTextFrame, note_id: int, text: str):
+        self.width = master.width
+        self.height = master.height
+        self.note_id = note_id
+        self.text = text
+
+        super().__init__(master, wrap=tk.WORD,
+                         width=self.width, height=self.height,
+                         font=('Times New Roman', 16))
+
+        self.insert(tk.END, self.text)
+
+        self.place(x=0, y=0)
+
+    def update_text(self, note_id: int, text: str):
+        self.note_id = note_id
+        self.delete('0.0', tk.END)
+        self.text = text
+        self.insert(tk.END, self.text)
+
+
 class NotesListFrame(tk.Canvas):
     width: int
     height: int
@@ -55,13 +99,18 @@ class NotesListFrame(tk.Canvas):
 
 
 class NoteTitleLabel(tk.Label):
+    note_id: int
     text_var: tk.StringVar
     bg_color_default = '#A4A4A4'
     bg_color_focused = '#808080'
+    note_text_area: NoteTextArea
 
-    def __init__(self, master: NotesListFrame, title: str, position: List[int]):
+    def __init__(self, master: NotesListFrame, note_id: int, title: str,
+                 position: List[int], note_text_area: NoteTextArea):
+        self.note_id = note_id
         self.text_var = tk.StringVar()
         self.text_var.set(title)
+        self.note_text_area = note_text_area
 
         super().__init__(master, textvariable=self.text_var)
 
@@ -69,6 +118,7 @@ class NoteTitleLabel(tk.Label):
 
         self.bind('<Enter>', self.__set_red_color)
         self.bind('<Leave>', self.__set_default_color)
+        self.bind('<Button-1>', self.__display_note_text)
 
         self.place(x=position[0], y=position[1])
 
@@ -77,3 +127,7 @@ class NoteTitleLabel(tk.Label):
 
     def __set_default_color(self, event):
         self.config(bg=self.bg_color_default)
+
+    def __display_note_text(self, event):
+        notes = handbookapi.get_versions(self.note_id)
+        self.note_text_area.update_text(notes[0].id, notes[0].text)
--- src/painter/painter.py
+++ src/painter/painter.py
@@ -6,12 +6,15 @@ def make_gui():
     main_window = gui.MainWindow()
     main_frame = gui.MainFrame(main_window)
     list_frame = gui.NotesListFrame(main_frame)
+    note_text_frame = gui.NoteTextFrame(main_frame, list_frame.width+15)
+    note_text_area = gui.NoteTextArea(note_text_frame, 0, '')
 
     notes = handbookapi.get_notes()
 
     for i, note in enumerate(notes):
         position = [15, i * 25 + 15]
 
-        gui.NoteTitleLabel(list_frame, note.title, position)
+        gui.NoteTitleLabel(list_frame, note.id, note.title,
+                           position, note_text_area)
 
     main_window.mainloop()
